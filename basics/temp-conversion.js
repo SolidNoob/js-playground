346d6863cc9c8ce97e16eb1a5f6187c70df0689c
SolidNoob <anthony.cammarata@hotmail.com>
2018-10-30 22:20:25 +0100

    temp conversion

---
+++ basics/temp-conversion.js
@@ -0,0 +1,8 @@
+let fahrenheit = 32
+let celsius = (fahrenheit - 32) * 5/9
+let kelvin = (fahrenheit + 459.67) * 5/9
+
+
+console.log('fahrenheit: ' + fahrenheit)
+console.log('celsius: ' + celsius)
+console.log('kelvin: ' + kelvin)
