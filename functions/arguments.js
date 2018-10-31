00449a14537c3d51de105c5e4358c11dbcd985e5
SolidNoob <anthony.cammarata@hotmail.com>
2018-10-31 23:13:16 +0100

    template string

--- functions/arguments.js
+++ functions/arguments.js
@@ -18,8 +18,13 @@ console.log(scoreText)
 
 
 let getTip = function (total, percentage = 0.2){
-    return total * percentage
+    let percent = percentage * 100
+    let tip = total * percentage
+    return `A ${percent}% tip on $${total} would be $${tip}`
 }
 
 let tip = getTip(80, 0.5)
+console.log(tip)
+
+tip = getTip(120, 0.1)
 console.log(tip)
