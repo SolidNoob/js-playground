1da1b6f6993f2b9ba822b2ed66d904813066af3f
SolidNoob <anthony.cammarata@hotmail.com>
2018-11-04 21:47:23 +0100

    const

---
+++ objects/const.js
@@ -0,0 +1,11 @@
+const isRaining = true
+// isRaining = false  --> throws error
+console.log(isRaining)
+
+
+const person = {
+    age: 27
+}
+person.age = 28
+//person = {} --> throws error
+console.log(person)
