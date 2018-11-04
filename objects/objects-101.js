bc9b8469f9097948f65c8baab0bd73f31225cc9a
SolidNoob <anthony.cammarata@hotmail.com>
2018-11-04 20:18:11 +0100

    objects

---
+++ objects/objects-101.js
@@ -0,0 +1,22 @@
+let myBook = {
+    title: '1984',
+    author: 'George Orwell',
+    pageCount: 326
+}
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+myBook.title = 'Animal Farm';
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+
+let person = {
+    name: 'Anthony',
+    age: 27,
+    location: 'Liège'
+}
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`)
+person.age++
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`)
