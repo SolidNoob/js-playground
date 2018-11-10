2d5df0b84520407337717973b6de1b9e8c25c274
SolidNoob <anthony.cammarata@hotmail.com>
2018-11-10 22:03:59 +0100

    array sorting

--- arrays/notes.js
+++ arrays/notes.js
@@ -13,6 +13,17 @@ const notes = [
     }
 ]
 
+const sortNotes = function (notes) {
+    notes.sort(function (a, b) {
+        if (a.title.toLowerCase() < b.title.toLowerCase()) {
+            return -1
+        } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+            return 1
+        } else {
+            return 0
+        }
+    })
+}
 
 const findNote = function (notes, noteTitle) {
     return notes.find(function (note) {
@@ -30,16 +41,10 @@ const findNotes = function (notes, query) {
     })
 }
 
-console.log(findNotes(notes, 'ex'))
+// console.log(findNotes(notes, 'ex'))
 
-/*
-const findNote = function (notes, noteTitle) {
-    const index = notes.findIndex(function (note){
-        return note.title.toLowerCase() === noteTitle.toLowerCase()
-    })
-    return notes[index]
-}
-*/
+// const note = findNote(notes, 'Habbits to work on')
+// console.log(note)
 
-const note = findNote(notes, 'Habbits to work on')
-console.log(note)
+sortNotes(notes)
+console.log(notes)
--- arrays/todo.js
+++ arrays/todo.js
@@ -21,23 +21,38 @@ const todos = [
     }
 ]
 
-const deleteTodo = function(todos, todoText){
-    const index = todos.findIndex(function(item){
+const sortTodos = function (todos) {
+    return todos.sort(function (a, b){
+        if (!a.completed && b.completed) {
+            return -1
+        } else if (a.completed && !b.completed) {
+            return 1
+        } else {
+            return 0
+        }
+    })
+}
+
+const deleteTodo = function (todos, todoText){
+    const index = todos.findIndex(function (item){
         return item.text.toLowerCase() === todoText.toLowerCase()
     })
 
-    if(index > -1) {
+    if (index > -1) {
         todos.splice(index, 1)
     }
 }
 
-const getThingsToDo = function(todos) {
-    return todos.filter(function(todo) {
+const getThingsToDo = function (todos) {
+    return todos.filter(function (todo) {
         return !todo.completed
     })
 }
 
-console.log(getThingsToDo(todos))
+sortTodos(todos)
+console.log(todos)
+
+//console.log(getThingsToDo(todos))
 
 //deleteTodo(todos, 'wash the dishes')
 //console.log(todos)
